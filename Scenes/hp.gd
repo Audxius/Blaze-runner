f978983737ce20c14188136ac2af735cac4b0449
Audxius <audxius@gmail.com>
2024-03-07 15:54:51 +0200

    minor bug fix with hp system

--- Scenes/hp.gd
+++ Scenes/hp.gd
@@ -1,18 +1,27 @@
 extends ColorRect
 
 var hp = 100;
+var timer = 1; #kiek laiko zaidejas turi iki scenos restarto
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(hp)
 	scale.x = hp*0.01
 	if Input.is_key_pressed(KEY_G):
-		damage(10)
-			
-func damage(amount):
-	if hp>=0:
-		hp-=amount
-		if hp>=0:
-			hp-=1
+		damage(5)
+	if Input.is_key_pressed(KEY_H):
+		heal(5)
+	#Jei zaidejas nebeturi gyvybes tasku scenai padaro restarta
 	if hp <=0:
-		get_tree().reload_current_scene()
+		timer-=delta
+		if timer<=0:
+			get_tree().reload_current_scene()	
+
+#Pasalino zaidejo gyvybes taskus nurodyta kieki
+func damage(amount):
+	if hp>0:
+		hp-=amount
+		
+#Papildo zaidejo gyvybes taskus nurodyta kieki
+func heal(amount):
+	if hp<100:
+		hp+=amount
