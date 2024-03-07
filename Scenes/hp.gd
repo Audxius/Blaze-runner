36534a699b3a87d05cc2daff3ae1956c97177bce
Audxius <audxius@gmail.com>
2024-03-07 15:37:17 +0200

    added damage function

--- Scenes/hp.gd
+++ Scenes/hp.gd
@@ -2,16 +2,13 @@ extends ColorRect
 
 var hp = 1000;
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	print(hp)
 	scale.x = hp*0.01
 	if Input.is_key_pressed(KEY_G):
-		if hp>=0:
-			hp-=1
+		damage(10)
+			
+func damage(amount):
+	if hp>=0:
+		hp-=amount
