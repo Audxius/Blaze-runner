25bf89ddbd247423db1d43cec316fedab58b7b76
Donatas <160858627+Medutis1@users.noreply.github.com>
2024-03-21 20:00:50 +0200

    added dagger functionality

--- Scenes/Weapons/dagger.gd
+++ Scenes/Weapons/dagger.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
-var kulka = preload("res://Scenes/laser.tscn")
+var animation
+var sound
+var inAnimation = false
+
+var damage = 25
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	animation = get_node("AttackAnim")
+	sound = get_node("AttackSound")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,7 +17,16 @@ func _process(delta):
 	look_at(get_global_mouse_position())
 
 	if Input.is_action_pressed("mouse_left"):
-		pass
+		if not inAnimation:
+			sound.play()
+			animation.play("dagger_attack")
+			inAnimation = true
+
+func _on_attack_anim_animation_finished(anim_name):
+	inAnimation = false
 
 
-
+func _on_body_entered(body):
+	if(inAnimation):
+		if body.has_method("take_damage"):
+			body.take_damage(damage)
