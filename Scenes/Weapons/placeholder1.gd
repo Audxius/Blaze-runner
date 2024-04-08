e6eaaf5f9bfd507f0dac50f3a755f8c464b6b71e
Donatas <160858627+Medutis1@users.noreply.github.com>
2024-04-08 17:45:36 +0300

    added firerate to submachinegun

--- Scenes/Weapons/placeholder1.gd
+++ Scenes/Weapons/placeholder1.gd
@@ -4,23 +4,29 @@ var kulka = preload("res://Scenes/laser.tscn")
 var kulka2 = preload("res://Scenes/Enemies/enemylaser.tscn")
 var player
 var shooting = 1
+var canShoot = true
+var timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_parent().get_parent().get_node("Player")
-
+	timer = $ShootTimer
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if get_parent().name == "Player":
-		if Input.is_action_pressed("mouse_left"):
+		if Input.is_action_pressed("mouse_left") and canShoot:
 			bullet()
+			canShoot = false
+			timer.start()
 		look_at(get_global_mouse_position())
-			
+		
 	else:
 		look_at(player.position)
-		if shooting == 0:
+		if shooting == 0 and canShoot:
 			bullet()
+			canShoot = false
+			timer.start()
 	
 	
 
@@ -37,6 +43,7 @@ func bullet():
 		get_parent().add_sibling(instance)
 
 
+
 func _on_notice_area_body_entered(body):
 	if body.name == "Player":
 		shooting = 0
@@ -46,3 +53,7 @@ func _on_notice_area_body_exited(body):
 	if body.name == "Player":
 		shooting = 1
 	
+
+
+func _on_shoot_timer_timeout():
+	canShoot = true
