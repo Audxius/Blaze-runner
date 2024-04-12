ea536c15f0deeeb9db09df3e0765546aaf38eb83
Audxius <audxius@gmail.com>
2024-04-12 22:29:47 +0300

    fixed score system bugs

--- Scenes/LootBox/AmmoLoot.gd
+++ Scenes/LootBox/AmmoLoot.gd
@@ -3,4 +3,5 @@ extends Node2D
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
+		body.get_node("CanvasLayer").get_node("score").add_score(150)
 		queue_free()
--- Scenes/LootBox/LootBox.gd
+++ Scenes/LootBox/LootBox.gd
@@ -29,7 +29,7 @@ func takeDamage(damage):
 		var loot = lootInstance
 		loot.position = position
 		get_parent().add_child(loot)
-		get_tree().get_root().get_node("Level1").get_node("Player").get_node("CanvasLayer").get_node("score").add_score(150)
+		#get_tree().get_root().get_node("Level1").get_node("Player").get_node("CanvasLayer").get_node("score").add_score(150)
 		queue_free()
 
 
