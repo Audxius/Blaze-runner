79076cb28031b3a03d7c4d3d64bf1f21ca532a85
Audxius <audxius@gmail.com>
2024-04-12 16:02:14 +0300

    added font n stuff

--- Scenes/score.gd
+++ Scenes/score.gd
@@ -4,7 +4,11 @@ var score = 0
 # Called when the node enters the scene tree for the first time.
 
 func _process(delta):
+	if Input.is_key_pressed(KEY_P):
+		add_score(1)
 	text = str(score)
 
 func add_score(amount):
 	score += amount
+	
+
