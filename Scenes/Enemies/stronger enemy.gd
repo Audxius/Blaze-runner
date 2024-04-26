0f208454a1c30d8aa9b42cdcbc22da44d45649e4
Audxius <audxius@gmail.com>
2024-04-26 23:29:13 +0300

    stronger enemy (not visually diff yet)

--- Scenes/Enemies/stronger enemy.gd
+++ Scenes/Enemies/stronger enemy.gd
@@ -2,45 +2,69 @@ extends CharacterBody2D
 
 var loadedWeapon
 
-@export var speed: = 200
+@export var speed: = 100
 @export var walkingTime = 3
 var timeWalked=0
-var direction: = 1
+var direction = 1:
+	set(val):
+		if val != direction: scale.x *= -1
+		direction = val
 var walking = 0
+var ogscale
 @onready var animationPlayer = $AnimationPlayer
+
 
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	 #Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	if walking == 0:
-		animationPlayer.play("Walking")
-		#walks left and right
-		timeWalked+=delta
-		if timeWalked>walkingTime:
-			timeWalked=0
-			direction*=-1
-			scale.x*=-1
-		position.x += speed*direction*delta
-		move_and_slide()
+	if(get_parent().get_node("Player").position.x>position.x):
+		
+		direction = 1
+
+		print("right")
+		
+
 	else:
-		animationPlayer.play("Idle")
+		direction = -1
 
-
-func replace_weapon(weaponPackedScene: PackedScene) -> void:
-	# Delete current weapon
-	if loadedWeapon:
-		var playerWeapon = get_node(str(loadedWeapon.name))
-		playerWeapon.queue_free()
-
-	# Add new weapon
-	var weaponInstance = weaponPackedScene.instance()
-	loadedWeapon = weaponInstance
-	add_child(weaponInstance)
-	
+		print("left")
+	position.x += speed*direction*delta	
+	move_and_slide()
+		#move_and_slide()
+		#
+		
+		
+	#if walking == 0:
+		#animationPlayer.play("Walking")
+		##walks left and right
+		#timeWalked+=delta
+		#if timeWalked>walkingTime:
+			#timeWalked=0
+			#direction*=-1
+			#scale.x*=-1
+		#position.x += speed*direction*delta
+		#move_and_slide()
+	#else:
+		#animationPlayer.play("Idle")
+#
+#
+#func replace_weapon(weaponPackedScene: PackedScene) -> void:
+	## Delete current weapon
+	#if loadedWeapon:
+		#var playerWeapon = get_node(str(loadedWeapon.name))
+		#playerWeapon.queue_free()
+#
+	## Add new weapon
+	#var weaponInstance = weaponPackedScene.instance()
+	#loadedWeapon = weaponInstance
+	#add_child(weaponInstance)
+	#
 
 
 
---
+++ Scenes/Enemies/strongerhp.gd
@@ -0,0 +1,33 @@
+extends ColorRect
+
+var hp = 500;
+var timer = 1; #kiek laiko zaidejas turi iki scenos restarto
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	scale.x = hp*0.01
+	if Input.is_key_pressed(KEY_J):
+		take_damage(5)
+	if Input.is_key_pressed(KEY_K):
+		heal(5)
+	#Jei zaidejas nebeturi gyvybes tasku scenai padaro restarta
+	if hp <=0:
+		get_parent().queue_free()
+		get_parent().get_parent().get_node("Player").get_node("CanvasLayer").get_node("score").add_score(150)
+
+#Pasalino zaidejo gyvybes taskus nurodyta kieki
+func take_damage(amount):
+	if hp>0:
+		hp-=amount
+		
+#Papildo zaidejo gyvybes taskus nurodyta kieki
+func heal(amount):
+	if hp<100:
+		hp+=amount
+
+
+func _on_hitbox_area_entered(area):
+	if area.name == "submachineGunBulletHitbox":
+		take_damage(10)
+	elif area.name == "pistolBulletHitbox":
+		take_damage(20)
