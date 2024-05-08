98df7f5e004736d2661bae8e5c42dede178ebe49
Donatas <160858627+Medutis1@users.noreply.github.com>
2024-05-08 23:49:19 +0300

    added score table to score menu

--- Scenes/ScoreScreen/ScoreScreenScript.gd
+++ Scenes/ScoreScreen/ScoreScreenScript.gd
@@ -1,25 +1,31 @@
 extends Node
 
 var dateLabels = []
+var timeLabels = []
 var scoreLabels = []
-var labelCount = 6
+var labelCount = 5
 #var dates = ["1", "2", "3"]
 #var scores = ["100", "200", "300"]
 #var offset = 0
 
 func _ready():
-	dateLabels.append($MarginContainer/HBoxContainer/VBoxContainer/DateLabel1);
-	dateLabels.append($MarginContainer/HBoxContainer/VBoxContainer/DateLabel2);
-	dateLabels.append($MarginContainer/HBoxContainer/VBoxContainer/DateLabel3);
-	dateLabels.append($MarginContainer/HBoxContainer/VBoxContainer/DateLabel4);
-	dateLabels.append($MarginContainer/HBoxContainer/VBoxContainer/DateLabel5);
-	dateLabels.append($MarginContainer/HBoxContainer/VBoxContainer/DateLabel6);
-	scoreLabels.append($MarginContainer2/HBoxContainer/VBoxContainer/ScoreLabel1)
-	scoreLabels.append($MarginContainer2/HBoxContainer/VBoxContainer/ScoreLabel2)
-	scoreLabels.append($MarginContainer2/HBoxContainer/VBoxContainer/ScoreLabel3)
-	scoreLabels.append($MarginContainer2/HBoxContainer/VBoxContainer/ScoreLabel4)
-	scoreLabels.append($MarginContainer2/HBoxContainer/VBoxContainer/ScoreLabel5)
-	scoreLabels.append($MarginContainer2/HBoxContainer/VBoxContainer/ScoreLabel6)
+	dateLabels.append($MarginContainerTable/HBoxContainer/VBoxContainerDates/DateLabel1);
+	dateLabels.append($MarginContainerTable/HBoxContainer/VBoxContainerDates/DateLabel2);
+	dateLabels.append($MarginContainerTable/HBoxContainer/VBoxContainerDates/DateLabel3);
+	dateLabels.append($MarginContainerTable/HBoxContainer/VBoxContainerDates/DateLabel4);
+	dateLabels.append($MarginContainerTable/HBoxContainer/VBoxContainerDates/DateLabel5);
+	
+	timeLabels.append($MarginContainerTable/HBoxContainer/VBoxContainer2Time/TimeLabel1);
+	timeLabels.append($MarginContainerTable/HBoxContainer/VBoxContainer2Time/TimeLabel2);
+	timeLabels.append($MarginContainerTable/HBoxContainer/VBoxContainer2Time/TimeLabel3);
+	timeLabels.append($MarginContainerTable/HBoxContainer/VBoxContainer2Time/TimeLabel4);
+	timeLabels.append($MarginContainerTable/HBoxContainer/VBoxContainer2Time/TimeLabel5);
+	
+	scoreLabels.append($MarginContainerTable/HBoxContainer/VBoxContainerScore/HBoxContainer1/ScoreLabel1)
+	scoreLabels.append($MarginContainerTable/HBoxContainer/VBoxContainerScore/HBoxContainer2/ScoreLabel2)
+	scoreLabels.append($MarginContainerTable/HBoxContainer/VBoxContainerScore/HBoxContainer3/ScoreLabel3)
+	scoreLabels.append($MarginContainerTable/HBoxContainer/VBoxContainerScore/HBoxContainer4/ScoreLabel4)
+	scoreLabels.append($MarginContainerTable/HBoxContainer/VBoxContainerScore/HBoxContainer5/ScoreLabel5)
 	update_info(Global.lastLevelFinished)
 
 
@@ -36,27 +42,33 @@ func load_level1_scores():
 	for i in range(labelCount):
 		if(i < Global.dates1.size()):
 			dateLabels[i].text = Global.dates1[i]
+			timeLabels[i].text = Global.time1[i]
 			scoreLabels[i].text = Global.highScoresStr1[i]
 		else:
 			dateLabels[i].text = " "
+			timeLabels[i].text = " "
 			scoreLabels[i].text = " "
 
 func load_level2_scores():
 	for i in range(labelCount):
 		if(i < Global.dates2.size()):
 			dateLabels[i].text = Global.dates2[i]
+			timeLabels[i].text = Global.time2[i]
 			scoreLabels[i].text = Global.highScoresStr2[i]
 		else:
 			dateLabels[i].text = " "
+			timeLabels[i].text = " "
 			scoreLabels[i].text = " "
 
 func load_level3_scores():
 	for i in range(labelCount):
 		if(i < Global.dates3.size()):
 			dateLabels[i].text = Global.dates3[i]
+			timeLabels[i].text = Global.time3[i]
 			scoreLabels[i].text = Global.highScoresStr3[i]
 		else:
 			dateLabels[i].text = " "
+			timeLabels[i].text = " "
 			scoreLabels[i].text = " "
 
 #	for i in range(5):
--- global.gd
+++ global.gd
@@ -15,16 +15,19 @@ var lastLevelFinished
 
 #level 1 score stuff
 var dates1 = []
+var time1 = []
 var highScoresStr1 = []
 var highScoresInt1 = []
 
 #level 2 score stuff
 var dates2 = []
+var time2 = []
 var highScoresStr2 = []
 var highScoresInt2 = []
 
 #level 3 score stuff
 var dates3 = []
+var time3 = []
 var highScoresStr3 = []
 var highScoresInt3 = []
 
@@ -36,14 +39,20 @@ func _ready():
 func save_level1_score():
 	if(highScoresInt1.size() == 0):
 		var date = Time.get_date_string_from_system()
+		var time = Time.get_time_string_from_system()
 		date = date.replace("-", "/")
+		time = time.left(5)
 		dates1.append(date)
+		time1.append(time)
 		highScoresInt1.append(score)
 		highScoresStr1.append(str(score))
 	elif(score > highScoresInt1.max()):
 		var date = Time.get_date_string_from_system()
+		var time = Time.get_time_string_from_system()
 		date = date.replace("-", "/")
+		time = time.left(5)
 		dates1.append(date)
+		time1.append(time)
 		highScoresInt1.append(score)
 		highScoresStr1.append(str(score))
 	save_data_to_disk()
@@ -53,14 +62,20 @@ func save_level1_score():
 func save_level2_score():
 	if(highScoresInt2.size() == 0):
 		var date = Time.get_date_string_from_system()
+		var time = Time.get_time_string_from_system()
 		date = date.replace("-", "/")
+		time = time.left(5)
 		dates2.append(date)
+		time2.append(time)
 		highScoresInt2.append(score)
 		highScoresStr2.append(str(score))
 	elif(score > highScoresInt2.max()):
 		var date = Time.get_date_string_from_system()
+		var time = Time.get_time_string_from_system()
 		date = date.replace("-", "/")
+		time = time.left(5)
 		dates2.append(date)
+		time2.append(time)
 		highScoresInt2.append(score)
 		highScoresStr2.append(str(score))
 	save_data_to_disk()
@@ -70,14 +85,20 @@ func save_level2_score():
 func save_level3_score():
 	if(highScoresInt3.size() == 0):
 		var date = Time.get_date_string_from_system()
+		var time = Time.get_time_string_from_system()
 		date = date.replace("-", "/")
+		time = time.left(5)
 		dates3.append(date)
+		time3.append(time)
 		highScoresInt3.append(score)
 		highScoresStr3.append(str(score))
 	elif(score > highScoresInt3.max()):
 		var date = Time.get_date_string_from_system()
+		var time = Time.get_time_string_from_system()
 		date = date.replace("-", "/")
+		time = time.left(5)
 		dates3.append(date)
+		time3.append(time)
 		highScoresInt3.append(score)
 		highScoresStr3.append(str(score))
 	save_data_to_disk()
@@ -86,25 +107,31 @@ func save_level3_score():
 
 func save_data_to_disk():
 	var dates1Save = JSON.stringify(dates1)
+	var time1Save = JSON.stringify(time1)
 	var highScoresInt1Save = JSON.stringify(highScoresInt1)
 	var highScoresStr1Save = JSON.stringify(highScoresStr1)
 	var dates2Save = JSON.stringify(dates2)
+	var time2Save = JSON.stringify(time2)
 	var highScoresInt2Save = JSON.stringify(highScoresInt2)
 	var highScoresStr2Save = JSON.stringify(highScoresStr2)
 	var dates3Save = JSON.stringify(dates3)
+	var time3Save = JSON.stringify(time3)
 	var highScoresInt3Save = JSON.stringify(highScoresInt3)
 	var highScoresStr3Save = JSON.stringify(highScoresStr3)
 	
 	var file = FileAccess.open("user://save_file.txt", FileAccess.WRITE)
 	file.store_string(dates1Save + "\n")
+	file.store_string(time1Save + "\n")
 	file.store_string(highScoresInt1Save + "\n")
 	file.store_string(highScoresStr1Save + "\n")
 	file.store_string(dates2Save + "\n")
+	file.store_string(time2Save + "\n")
 	file.store_string(highScoresInt2Save + "\n")
 	file.store_string(highScoresStr2Save + "\n")
 	file.store_string(dates3Save + "\n")
+	file.store_string(time3Save + "\n")
 	file.store_string(highScoresInt3Save + "\n")
-	file.store_string(highScoresStr3Save + "\n")
+	file.store_string(highScoresStr3Save)
 	file.close()
 
 func load_data_from_disk():
@@ -112,22 +139,28 @@ func load_data_from_disk():
 		var file = FileAccess.open("user://save_file.txt", FileAccess.READ)
 		
 		var date1Line = file.get_line()
+		var time1Line = file.get_line()
 		var scoreInt1Line = file.get_line()
 		var scoreStr1Line = file.get_line()
 		var date2Line = file.get_line()
+		var time2Line = file.get_line()
 		var scoreInt2Line = file.get_line()
 		var scoreStr2Line = file.get_line()
 		var date3Line = file.get_line()
+		var time3Line = file.get_line()
 		var scoreInt3Line = file.get_line()
 		var scoreStr3Line = file.get_line()
 		
 		dates1 = JSON.parse_string(date1Line)
+		time1 = JSON.parse_string(time1Line)
 		highScoresInt1 = JSON.parse_string(scoreInt1Line)
 		highScoresStr1 = JSON.parse_string(scoreStr1Line)
 		dates2 = JSON.parse_string(date2Line)
+		time2 = JSON.parse_string(time2Line)
 		highScoresInt2 = JSON.parse_string(scoreInt2Line)
 		highScoresStr2 = JSON.parse_string(scoreStr2Line)
 		dates3 = JSON.parse_string(date3Line)
+		time3 = JSON.parse_string(time3Line)
 		highScoresInt3 = JSON.parse_string(scoreInt3Line)
 		highScoresStr3 = JSON.parse_string(scoreStr3Line)
 		
